7936851195df393541a04fc3ddb537d80222829e
shurik <shurik@skotch.nirvana>
2021-12-05 14:37:25 +0300

    2

---
+++ 2lab/1lab_spatial_smoothing.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+from main import *
+
+def new_apply_func(pix_img, mask, point:Point_type):
+    mid_mask = (len(mask)//2, len(mask[0])//2)
+    pixs = np.empty((len(mask), len(mask[0]), len(pix_img[point])))
+    for imask, mask_row in enumerate(mask):
+        for jmask, mask_val in enumerate(mask_row):
+            try:
+                tmp = np.array(pix_img[point[0]+jmask-mid_mask[0], point[1]+imask-mid_mask[1]]) * np.array(mask_val)
+                #print(tmp)
+            except IndexError:
+                tmp = np.zeros(len(pix_img[point]))
+            pixs[imask, jmask] = tmp
+
+    if np.sum(pixs) >= 6*255*len(pix_img[point]):
+        return (255, 255, 255)
+    elif np.sum(pixs) <= 2*255*len(pix_img[point]):
+        return (0,0,0)
+    else:
+        return pix_img[point]
+
+
+@print_durations
+def spatial_smoothing(image, draw):
+    res = image.copy()
+    mask = [[1,1,1],
+            [1,0,1],
+            [1,1,1]]
+
+    for pos, p in pixel_gen(res, mask=mask, apply_func=new_apply_func):
+        draw.point(pos, p)
+
+
+    res.save("res_"+name+"_1.jpg", "JPEG")
+    return res
+
+
+if __name__ == "__main__":
+    main(spatial_smoothing)
+
+
